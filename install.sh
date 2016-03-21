f6f2b479a24d60b3914a401eeb1aeeddaea42fec
Iyyel <iyyelsec@gmail.com>
2016-03-21 20:03:28 +0100

    Updated dotfiles

--- install.sh
+++ install.sh
@@ -27,11 +27,9 @@ ask() {
 
 dir=`pwd`
 
-echo "Don't forget to manually cp mbpfan.conf to /etc/mbpfan.conf!"
-
 if ask "Install symlink for .zshrc?" Y; then
-  ln -sf ${dir}/.zshrc ${HOME}/.zshrc
-  ln -sf ${dir}/.aliases ${HOME}/.aliases
+  ln -sfn ${dir}/.zshrc ${HOME}/.zshrc
+  ln -sfn ${dir}/.aliases ${HOME}/.aliases
 fi
 
 if ask "Install symlink for .config/nvim?" Y; then
@@ -39,15 +37,15 @@ if ask "Install symlink for .config/nvim?" Y; then
 fi
 
 if ask "Install symlink for .Xresources?" Y; then
-  ln -sf ${dir}/.Xresources ${HOME}/.Xresources
+  ln -sfn ${dir}/.Xresources ${HOME}/.Xresources
 fi
 
 if ask "Install symlink for .xinitrc?" Y; then
-  ln -sf ${dir}/.xinitrc ${HOME}/.xinitrc
+  ln -sfn ${dir}/.xinitrc ${HOME}/.xinitrc
 fi
 
 if ask "Install symlink for .config/i3/i3blocks.conf?" Y; then
-  ln -sf ${dir}/.config/i3/i3blocks.conf ${HOME}/.config/i3/i3blocks.conf
+  ln -sfn ${dir}/.config/i3/i3blocks.conf ${HOME}/.config/i3/i3blocks.conf
 fi
 
 if ask "Install symlink for .config/i3/config?" Y; then
@@ -55,7 +53,7 @@ if ask "Install symlink for .config/i3/config?" Y; then
 fi
 
 if ask "Install symlink for .local/share/fonts?" Y; then
-  ln -sf ${dir}/.local/share/fonts ${HOME}/.local/share/fonts
+  ln -sfn ${dir}/.local/share/fonts ${HOME}/.local/share/fonts
 fi
 
 if ask "Install symlink for .scripts?" Y; then
@@ -63,5 +61,9 @@ if ask "Install symlink for .scripts?" Y; then
 fi
 
 if ask "Install symlink for .zlogin?" Y; then
-  ln -sf ${dir}/.zlogin ${HOME}/.zlogin
+  ln -sfn ${dir}/.zlogin ${HOME}/.zlogin
+fi
+
+if ask "Install symlink for /etc/mbpfan.conf?" Y; then
+  sudo ln -sfn ${dir}/mbpfan.conf /etc/mbpfan.conf
 fi
